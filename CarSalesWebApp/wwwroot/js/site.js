158fe9e9da2be804af988e9420a8481e8da94aca
jeccle <jeccles48@gmail.com>
2022-11-04 05:54:52 +0800

    Made webpage look nice, fixed up code to fulfill logic requirements.

--- CarSalesWebApp/wwwroot/js/site.js
+++ CarSalesWebApp/wwwroot/js/site.js
@@ -2,13 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 
-
+var allProducts;
 
 // Write your JavaScript code.
 function populateAllBrandBox(products) {
     let brandRow = document.getElementById("brand-row");
-    let brandButton = document.createElement("select");
-    brandButton.id = brandButton.name = "brand-drop-button";
+    let brandButton = document.getElementById("brand-drop-button");
+    brandButton.options.length = 0;
+    //let brandButton = document.getElementById("select");
+    //brandButton.id = brandButton.name = "brand-drop-button";
     let brandContent = document.createElement("div");
     brandContent.className = "drop-content";
     
@@ -18,6 +20,7 @@ function populateAllBrandBox(products) {
         if (!brandNames.includes(product.brand))
             brandNames.push(product.brand);
     });
+
     // populate each row with values.
     brandNames.forEach(brand => {
         let brandAnchor = document.createElement("option");
@@ -32,10 +35,10 @@ function populateAllBrandBox(products) {
 function populateAllModelBox(products) {
     let modelRow = document.getElementById("model-row");
 
-    let modelButton = document.createElement("select");
-    modelButton.id = "model-drop-button";
-    modelButton.name = "model-drop-button";
-
+    let modelButton = document.getElementById("model-drop-button");
+    //modelButton.id = "model-drop-button";
+   // modelButton.name = "model-drop-button";
+    modelButton.options.length = 0;
     let modelContent = document.createElement("div");
     modelContent.className = "drop-content";
 
@@ -59,7 +62,7 @@ function populateAllModelBox(products) {
 
 
     modelRow.appendChild(modelButton, modelContent);
-    addEventListeners();
+    
 }
 
 function addEventListeners() {
@@ -67,37 +70,34 @@ function addEventListeners() {
     let modelButton = document.getElementById("model-drop-button");
 
 
-    brandButton.addEventListener('change', () => {
+    brandButton.addEventListener('change', (e) => {
         clearProducts();
         setSearchTerm();
         //filterProducts("brand=" + brandButton.value + "&model=" + modelButton.value);
     });
 
-    modelButton.addEventListener('change', () => {
+    modelButton.addEventListener('change', (e) => {
         clearProducts();
         setSearchTerm();
         //filterProducts("brand=" + brandButton.value + "&model=" + modelButton.value);
     });
 
 }
-function specificBrandModelBox(brand, products) {
-    let modelBox = document.getElementById("model-row");
-    modelBox.removeChild(document.getElementById("model-drop-button"));
+function specificBrandModelBox(products) {
+    //let modelBox = document.getElementById("model-row");
+    //modelBox.removeChild(document.getElementById("model-drop-button"));
     let modelRow = document.getElementById("model-row");
-    let modelButton = document.createElement("select");
-    modelButton.id = "model-drop-button";
-    modelButton.name = "model-drop-button";
-
+    
+    //let modelButton = document.createElement("select");
+    let modelButton = document.getElementById("model-drop-button");
+    modelButton.options.length = 0;
+    //modelButton.id = "model-drop-button";
+    //modelButton.name = "model-drop-button";
+    
     let modelContent = document.createElement("div");
     modelContent.className = "drop-content";
 
-    let modelNames = [""];
-
-    // Push all products that are associated with specified brand
-    products.forEach(product => {
-        if (!modelNames.includes(product.model) && product.brand == brand)
-            modelNames.push(product.model);
-    });
+    let modelNames = populateModelNames(document.getElementById("brand-drop-button").value);
 
     modelNames.forEach(model => {
         let modelAnchor = document.createElement("option")
@@ -108,6 +108,23 @@ function specificBrandModelBox(brand, products) {
     modelRow.appendChild(modelButton, modelContent);
 }
 
+function populateModelNames(brand) {
+
+    let modelNames = [""];
+
+    // Push all products that are associated with specified brand
+
+    allProducts.forEach(product => {
+        if (!modelNames.includes(product.model) && product.brand == brand)
+            modelNames.push(product.model);
+    });
+    return modelNames;
+}
+
+function setGlobalProducts(products) {
+    allProducts = products;
+}
+
 function dropDownExists() {
     if (document.body.contains(document.getElementById("brand-drop-button")))
         return true;
@@ -116,16 +133,19 @@ function dropDownExists() {
 }
 
 function showProducts(products) {
-    if (!dropDownExists()) {
+    let brandDropDown = document.getElementById("brand-drop-button");
+    let modelDropDown = document.getElementById("model-drop-button");
+    if (modelDropDown.value.length == 0)
+        setGlobalProducts(products);
+    if (brandDropDown.options.length == 0) {
         // Checks if the dropdown menus have already been loaded in full.
         populateAllBrandBox(products);
         populateAllModelBox(products);
-        
+        addEventListeners();
     }   
-    else {
-        brandDropDown = document.getElementById("brand-drop-button");
-        if (brandDropDown.value.length > 0)                         // This will set the contents of the Models dropdown 
-            specificBrandModelBox(brandDropDown.value, products);   // to display those related to the specified brand.
+    else if (brandDropDown.value.length > 0) {
+                                // This will set the contents of the Models dropdown 
+        specificBrandModelBox(brandDropDown.value, products);   // to display those related to the specified brand.
     }
     let productsList = document.getElementById("products-container");
     var count = 0;  // Count pointer tracks how many products in the row.
@@ -219,15 +239,3 @@ function filterProducts(searchTerm) {
 
 }
 
-function getProducts(searchTerm) {
-
-    let productsList = document.getElementById("products-container");
-    url = "https://localhost:7096/api/Products";
-    fetch(url)
-        .then(response => response.json())
-        .then(data => showProducts(data, productsList))
-        .catch(ex => {
-            alert("error ");
-            console.error(ex);
-        });
-}
